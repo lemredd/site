de18f8f2e0e1d824d2772fd8ba42052b41f376e9
Lem Redd <lmoa.jhdp@gmail.com>
2023-05-10 11:47:58 +0800

    config(log): import changelog configuration
    from https://github.com/lemredd/rock-paper-scissors/

---
+++ changelog.config.ts
@@ -0,0 +1,9 @@
+export default {
+	"github": "lemredd/rock-paper-scissors",
+	"types": {
+		"dx": { "title": "🔦 Development Experience" },
+		"unit": { "title": "🦠 Unit Tests" },
+		"intg": { "title": "🧪 Integration Tests" },
+		"deps": { "title": "🔗 Dependencies" }
+	}
+}
