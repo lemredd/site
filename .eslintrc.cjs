0a446669da799f07777d82ea58a593d2555d5469
Lem Redd <lmoa.jhdp@gmail.com>
2023-07-18 22:45:48 +0800

    config(lint): remove `svelte` related options
    I plan to make branches for each of my desired frameworks. Once a branch
    for `svelte` is created, this commit will be reverted.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,29 +3,18 @@ module.exports = {
 	"extends": [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
-		"plugin:svelte/recommended"
 	],
 	"parser": "@typescript-eslint/parser",
 	"plugins": ["@typescript-eslint"],
 	"parserOptions": {
 		"sourceType": "module",
 		"ecmaVersion": 2020,
-		"extraFileExtensions": [".svelte"]
 	},
 	"env": {
 		"browser": true,
 		"es2017": true,
 		"node": true
 	},
-	"overrides": [
-		{
-			"files": ["*.svelte"],
-			"parser": "svelte-eslint-parser",
-			"parserOptions": {
-				"parser": "@typescript-eslint/parser"
-			}
-		}
-	],
 	"rules": {
 		"max-lines": "warn",
 		"indent": [
