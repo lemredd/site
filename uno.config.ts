8d3171ab96a2e56d4cecd348532f67f711669621
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-07-21 14:15:28 +0800

    config(unocss): `presetWind`

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig, presetTypography } from "unocss";
+import { defineConfig, presetTypography, presetWind } from "unocss";
 import transformerDirectives from "@unocss/transformer-directives";
 
 export default defineConfig({
 	"presets": [
-		presetTypography()
+		presetTypography(),
+		presetWind()
 	],
 	"transformers": [
 		transformerDirectives()
