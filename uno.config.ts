aee626248a01055beebb67bee24842d2841f1eaa
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-07-21 13:59:00 +0800

    config(unocss): use `transformerDirectives`

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "unocss";
+import transformerDirectives from "@unocss/transformer-directives";
 
 export default defineConfig({
-
+	"transformers": [
+		transformerDirectives()
+	]
 });
