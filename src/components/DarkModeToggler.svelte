537c6b7b1213c2e65e58e3bfc24f0595f02335f2
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-09 17:02:12 +0800

    refactor(component): make `viewport` svelte props

--- src/components/DarkModeToggler.svelte
+++ src/components/DarkModeToggler.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 type ColorScheme = string|null
+type Viewport = "mobile"|"desktop";
+
+export let viewport: Viewport = "mobile";
 
 const dark_mode_checkboxes = document.querySelectorAll(".dark-mode-toggler") as NodeListOf<HTMLInputElement>;
 const { "documentElement": root } = document;
@@ -28,8 +31,8 @@ checkbox_mobile?.addEventListener("change", event => toggle_checkboxes_simultane
 checkbox_desktop?.addEventListener("change", event => toggle_checkboxes_simultaneously(event, checkbox_mobile as HTMLInputElement));
 </script>
 
-<input type="checkbox" id={`dark-mode-toggler-${viewport}`} class="dark-mode-toggler" hidden />
-<label for={`dark-mode-toggler-${viewport}`} tabindex="0" class="dark-mode-toggler-icon">
+<input type="checkbox" id="dark-mode-toggler-{viewport}" class="dark-mode-toggler" hidden />
+<label for="dark-mode-toggler-{viewport}"  tabindex="0" class="dark-mode-toggler-icon">
 	<div class="icon on i-material-symbols:dark-mode" />
 	<div class="icon off i-material-symbols:light-mode" />
 </label>
