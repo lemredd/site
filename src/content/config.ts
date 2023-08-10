53b109c94cf305f8c33f693145fd2f134b042c4e
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-10 17:13:39 +0800

    config(content): make `category_item` validator

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,15 @@
 import { z as Validator, defineCollection } from "astro:content";
 
+const category_item = Validator.object({
+	"item-title": Validator.string(),
+	"item-link": Validator.string().optional() // TODO: validate link
+});
+
 const about = defineCollection({
 	"type": "data",
 	"schema": Validator.object({
 		"category": Validator.string(),
+		"category-items": Validator.array(category_item)
 	})
 });
 
