77487b7c4b0071db6814090531e404ea3b1c668f
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-11 22:54:56 +0800

    config(content): refine `item-subcategory`

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,7 +10,7 @@ const valid_subcategories = [
 type ValidSubcategory = typeof valid_subcategories[number];
 const is_valid_subcategory = (subcategory: string): subcategory is ValidSubcategory => valid_subcategories.indexOf(subcategory as ValidSubcategory) !== -1;
 const category_item = Validator.object({
-	"item-title": Validator.string(),
+	"item-title": Validator.string().refine(is_valid_subcategory),
 	"item-subcategory": Validator.string(),
 	"item-link": Validator.string().url().optional()
 });
