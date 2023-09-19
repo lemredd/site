dd6ad48914801d1368e24d0bdd219d18be62067a
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-19 10:49:26 +0800

    config(unocss): make `site` colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,19 @@
 import transformerDirectives from "@unocss/transformer-directives";
 import { defineConfig, presetTypography, presetUno } from "unocss";
 
+// site colors
+const site = {
+	"primary": "#DD2121",
+	"secondary": "#FFDB89",
+	"black": "#1F1E1C",
+	"black-75": "#1F1E1CC0",
+	"black-50": "#1F1E1C80",
+	"black-25": "#1F1E1C40",
+	"black-10": "#1F1E1C1A",
+	"black-05": "#1F1E1C0D",
+	"footer-black": "#33322E"
+};
+
 export default defineConfig({
 	"presets": [
 		presetTypography(),
@@ -8,5 +21,8 @@ export default defineConfig({
 	],
 	"transformers": [
 		transformerDirectives()
-	]
+	],
+	"theme": {
+		"colors": { site }
+	}
 });
