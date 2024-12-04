d892ff01144ba400c22f25f5ad0d6a135c711a21
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-04 20:37:06 +0800

    chore: remove everything v1 related

--- changelog/helper/validateReleaseTypeArg.ts
+++
@@ -1,8 +0,0 @@
-import type { ReleaseType } from "../types.js"
-
-import { releaseTypes } from "../constants.js"
-
-
-export default function(arg: string): boolean {
-	return releaseTypes.includes(arg as ReleaseType)
-}
