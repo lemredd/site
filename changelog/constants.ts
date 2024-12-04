a84c505ceff4c65eb002bbeda8331db569c0f4f9
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-04 20:37:06 +0800

    chore: remove everything v1 related

--- changelog/constants.ts
+++
@@ -1,1 +0,0 @@
-export const releaseTypes = ["patch", "minor", "major"] as const
--- changelog/types.ts
+++
@@ -1,4 +0,0 @@
-import { releaseTypes } from "./constants"
-
-export type ReleaseType = typeof releaseTypes[number]
-export type GeneratorMode = "dry-run" | "write"
