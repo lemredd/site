fe5b9b0eab949f9620b066a828a1703ed9b6af04
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-07 15:45:39 +0800

    feat(main): serve static files

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
+import { serveFile } from "@std/http/file-server";
+
 import nunjucks from "https://deno.land/x/nunjucks@3.2.4/mod.js";
 
 nunjucks.configure("html", { autoescape: true, noCache: true });
 
-Deno.serve((_req) => {
+Deno.serve(async (_req) => {
+  const url = new URL(_req.url);
+
+  if (url.pathname.startsWith("/static")) {
+    return await serveFile(_req, `./${url.pathname}`);
+  }
+
   return new Response(
     nunjucks.render("index.html", { title: "Hello", message: "World" }),
     { headers: { "content-type": "text/html" } },
