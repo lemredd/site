1f2b9f1a89de0c43e421dc11f72d13d5bb3c1845
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-08 20:42:10 +0800

    config(tailwind): add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+/** @type {import('npm:tailwindcss').Config} */
 export default {
-	content: ["./html/**/*.html"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./html/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2EEBAA",
+        fg: "#FFFFFF",
+        bg: "#000000",
+      },
+    },
+  },
+  plugins: [],
 };
