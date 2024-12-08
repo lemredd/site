2a2dd04d98d72ce2a870cea569cf9b24e01b64af
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-08 09:00:39 +0800

    style(main): indent with tabs

--- main.ts
+++ main.ts
@@ -5,14 +5,14 @@ import nunjucks from "https://deno.land/x/nunjucks@3.2.4/mod.js";
 nunjucks.configure("html", { autoescape: true, noCache: true });
 
 Deno.serve(async (_req) => {
-  const url = new URL(_req.url);
+	const url = new URL(_req.url);
 
-  if (url.pathname.startsWith("/static")) {
-    return await serveFile(_req, `./${url.pathname}`);
-  }
+	if (url.pathname.startsWith("/static")) {
+		return await serveFile(_req, `./${url.pathname}`);
+	}
 
-  return new Response(
-    nunjucks.render("index.html", { title: "Hello", message: "World" }),
-    { headers: { "content-type": "text/html" } },
-  );
+	return new Response(
+		nunjucks.render("index.html", { title: "Hello", message: "World" }),
+		{ headers: { "content-type": "text/html" } },
+	);
 });
