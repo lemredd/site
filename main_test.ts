c8e5391788301a762a24fd7f90bcbbdd909ac17f
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-14 04:48:51 +0800

    test(main): test 404s

--- main_test.ts
+++ main_test.ts
@@ -1,2 +1,13 @@
-Deno.test(function() {
+/**
+ * TODO:
+ * 1. Run web server on a separate thread
+ */
+
+import { assert } from "@std/assert";
+import { mainHandler } from "./main.ts";
+
+Deno.test("404 for unknown routes", async () => {
+  const request = new Request("http://localhost:8000/unknown");
+  const response = await mainHandler(request);
+  assert(response.status === 404);
 });
