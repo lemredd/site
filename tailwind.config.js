c8244868282956f0e5739b711d806ce656734b32
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-15 14:48:20 +0800

    config(tailwind): add `inter` font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ export default {
   content: ["./html/**/*.html"],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
       colors: {
         primary: "#2EEBAA",
         fg: "#FFFFFF",
+        "fg-2": "#999",
         bg: "#000000",
       },
     },
