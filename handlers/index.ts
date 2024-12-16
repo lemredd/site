0958e4e76051b057051863acdbdf936ace5e578d
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-17 07:48:17 +0800

    refactor(handler): handle by hierarchy
    1. By pattern
    2. By static files
    3. 404 by default

--- handlers/index.ts
+++ handlers/index.ts
@@ -11,21 +11,25 @@ const ROUTES: Record<string, RouteHandler> = {
   "/contact": contactHandler,
 };
 
-const handleByURLPattern = (request: Request): Response => {
+const handleByURLPattern = (request: Request): [Response, boolean] => {
+  // TODO: use `@std/http/route` once stable
   const url = new URL(request.url);
   for (const pathname in ROUTES) {
     const pattern = new URLPattern({ pathname });
-    if (pattern.test(url)) return ROUTES[pathname](request);
+    if (pattern.test(url)) return [ROUTES[pathname](request), true];
   }
 
-  return new Response("Not Found", { status: 404 });
+  return [new Response("Not Found", { status: 404 }), false];
 };
 
 export const mainHandler = (async (request: Request) => {
-  const url = new URL(request.url);
-  if (url.pathname.startsWith("/static") || url.pathname === "/favicon.ico") {
-    return await serveFile(request, `./${url.pathname}`);
-  }
+  const [response, matched] = handleByURLPattern(request);
+  if (matched) return response;
 
-  return handleByURLPattern(request);
+  try {
+    const url = new URL(request.url);
+    return await serveFile(request, `./static${url.pathname}`);
+  } catch {
+    return response; // 404 by default
+  }
 }) satisfies Deno.ServeHandler;
