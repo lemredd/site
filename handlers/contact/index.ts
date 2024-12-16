0b27f3a90d227c3ce094101a70ba0e03de2fc32d
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-17 06:59:46 +0800

    refactor(handler): use `render` utility

--- handlers/contact/index.ts
+++ handlers/contact/index.ts
@@ -1,11 +1,9 @@
-import { Environment as TemplateEnvironment } from "npm:@types/nunjucks";
-
+import { render } from "@/handlers/utils.ts";
 import { RouteHandler } from "@/handlers/types.ts";
 
-export const contactHandler = (template: TemplateEnvironment) =>
-  ((request: Request): Response => {
-    return new Response(
-      template.render("contact/index.html", { request, title: "Hello" }),
-      { headers: { "content-type": "text/html" } },
-    );
-  }) satisfies RouteHandler;
+export const contactHandler = ((request: Request): Response => {
+  return render({
+    name: "contact/index.html",
+    context: { request, title: "Hello" },
+  });
+}) satisfies RouteHandler;
