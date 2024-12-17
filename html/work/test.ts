c0a9411d74c07226b6cdd127be14063c46bd338d
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-18 07:36:15 +0800

    test(html): it displays skeletons initially

--- html/work/test.ts
+++ html/work/test.ts
@@ -6,13 +6,11 @@ import { load } from "cheerio";
 import { template } from "@/handlers/utils.ts";
 
 describe("Work: Projects", () => {
-  it("displays skeleton initially", async () => {
-    const context = { initial: true };
-
-    const rendered = template.render("work/projects.hx.html", context);
+  it("displays skeletons initially", () => {
+    const rendered = template.render("work/projects.hx.html", {});
     const $ = load(rendered);
 
     expect($("li").length).toBe(3);
-    expect($("li").hasClass(""));
+    expect($("li").hasClass("animate-pulse")).toBe(true);
   });
 });
