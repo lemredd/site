61a3c8a96719a4ef71439761b8537325d37692a1
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-19 08:08:39 +0800

    test(handler): stub fetching projects

--- handlers/work/test.ts
+++ handlers/work/test.ts
@@ -1,20 +1,34 @@
 import { expect } from "@std/expect";
+import { stub } from "@std/testing/mock";
 import { describe, it } from "@std/testing/bdd";
-
-import { load } from "cheerio";
 
 import { mainHandler } from "@/handlers/index.ts";
 
+const PROJECTS_FIXTURE = [
+  { id: 1 },
+  { id: 2 },
+];
+
 describe("Work: API Integration", () => {
-  it("lists three projects", async () => {
+  it("lists projects from GitHub", async () => {
+    const mockHeaders = {
+      "Content-Type": "application/json",
+    } satisfies HeadersInit;
+    const mockResponse = new Response(
+      JSON.stringify(PROJECTS_FIXTURE) satisfies BodyInit,
+      { headers: mockHeaders },
+    );
+    using fetchStub = stub(
+      globalThis,
+      "fetch",
+      () => Promise.resolve(mockResponse),
+    );
+
     const response = await mainHandler(
       new Request("http://localhost:8000/work/projects"),
     );
 
     expect(response.status).toBe(200);
-
-    const $ = load(await response.text());
-    expect($("li").length).toBe(3);
-    expect($("ul").attr("hx-swap-oob")).toBe("true");
+    console.log(fetchStub);
   });
 });
