ac9d1fc86fce3ea7dab6bb82c4ca05966cbcc869
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-24 12:48:54 +0800

    feat(handler): post contact form

--- handlers/contact/index.ts
+++ handlers/contact/index.ts
@@ -1,7 +1,29 @@
 import { render } from "@/handlers/utils.ts";
 import { RouteHandler } from "@/handlers/types.ts";
 
-export const contactHandler = ((request: Request): Response => {
+const submitContactForm = (async (request: Request): Promise<Response> => {
+  const formData = await request.formData();
+  // TODO: validate required fields
+  //const formDataObject = Object.fromEntries(formData);
+
+  const response = await fetch(
+    `https://script.google.com/macros/s/${
+      Deno.env.get("GOOGLE_APPS_SCRIPT_DEPLOYMENT_ID")
+    }/exec`,
+    {
+      method: "POST",
+      body: formData,
+    },
+  );
+
+  if (!response.ok) {
+    // TODO: return error hypermedia
+  }
+  return new Response("TODO");
+}) satisfies RouteHandler;
+
+export const contactHandler = (async (request: Request): Promise<Response> => {
+  if (request.method === "POST") return await submitContactForm(request);
   return render({
     name: "contact/index.html",
     context: { request, title: "Contact" },
