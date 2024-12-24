86b92aba8a66c539a518c548d338c909c9b804c9
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-24 20:05:30 +0800

    feat(handler): validate `/contact` form

--- handlers/contact/index.ts
+++ handlers/contact/index.ts
@@ -1,10 +1,19 @@
 import { render } from "@/handlers/utils.ts";
 import { RouteHandler } from "@/handlers/types.ts";
 
+const validateContactForm = (form: Record<string, FormDataEntryValue>) => {
+  const isValid = form.name && form.email && form.message;
+  if (!isValid) throw new Error("Invalid form");
+  return isValid;
+};
 const submitContactForm = (async (request: Request): Promise<Response> => {
   const formData = await request.formData();
-  // TODO: validate required fields
-  //const formDataObject = Object.fromEntries(formData);
+  try {
+    validateContactForm(Object.fromEntries(formData));
+  } catch {
+    return new Response("Invalid form", { status: 400 });
+  }
+  // TODO: integrate Web3Forms
 
   const response = await fetch(
     `https://script.google.com/macros/s/${
@@ -22,10 +31,12 @@ const submitContactForm = (async (request: Request): Promise<Response> => {
   return new Response("TODO");
 }) satisfies RouteHandler;
 
-export const contactHandler = (async (request: Request): Promise<Response> => {
+const contactHandler = (async (request: Request): Promise<Response> => {
   if (request.method === "POST") return await submitContactForm(request);
   return render({
     name: "contact/index.html",
     context: { request, title: "Contact" },
   });
 }) satisfies RouteHandler;
+
+export { contactHandler, submitContactForm };
