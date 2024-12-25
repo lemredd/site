2b1a5838b874d2cce2bc0264f114e9bc0587d27f
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-25 13:04:28 +0800

    feat(handler): verify turnstile token

--- handlers/contact/index.ts
+++ handlers/contact/index.ts
@@ -6,6 +6,33 @@ const validateContactForm = (form: Record<string, FormDataEntryValue>) => {
   if (!isValid) throw new Error("Invalid form");
   return isValid;
 };
+
+export const postToAppScript = async (body: FormData): Promise<Response> =>
+  await fetch(
+    `https://script.google.com/macros/s/${
+      Deno.env.get("GOOGLE_APPS_SCRIPT_DEPLOYMENT_ID")
+    }/exec`,
+    {
+      method: "POST",
+      body,
+    },
+  );
+
+export const verifyTurnstileToken = async (token: string) => {
+  const verifyURL = new URL(
+    "/turnstile/v0/siteverify",
+    "https://challenges.cloudflare.com",
+  );
+  const body = new FormData();
+  body.append("secret", String(Deno.env.get("CF_TURNSTILE_SECRET_KEY")));
+  body.append("token", token);
+  const response = await fetch(verifyURL, {
+    method: "POST",
+    body,
+  });
+  return response.json();
+};
+
 const submitContactForm = (async (request: Request): Promise<Response> => {
   const formData = await request.formData();
   try {
@@ -15,19 +42,9 @@ const submitContactForm = (async (request: Request): Promise<Response> => {
   }
   // TODO: integrate Web3Forms
 
-  const response = await fetch(
-    `https://script.google.com/macros/s/${
-      Deno.env.get("GOOGLE_APPS_SCRIPT_DEPLOYMENT_ID")
-    }/exec`,
-    {
-      method: "POST",
-      body: formData,
-    },
-  );
+  //const response = await postToAppScript(formData);
+  //if (!response.ok) { TODO: return error hypermedia }
 
-  if (!response.ok) {
-    // TODO: return error hypermedia
-  }
   return new Response("TODO");
 }) satisfies RouteHandler;
 
