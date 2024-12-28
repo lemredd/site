a7c2718f42254eb8971cda8a3e076455598372f1
Lem Redd <lmoa.jhdp@gmail.com>
2024-12-28 20:40:29 +0800

    feat(main): listen to exposed port

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 import { mainHandler } from "@/handlers/index.ts";
 
-Deno.serve({ port: Number(Deno.env.get("PORT") ?? 8000) }, mainHandler);
+Deno.serve(
+  { port: Number(Deno.env.get("PORT") ?? 8000), hostname: "0.0.0.0" },
+  mainHandler,
+);
