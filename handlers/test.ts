ee76e974e9688b2ed2ed56441cd1206148736311
Lem Redd <lmoa.jhdp@gmail.com>
2025-01-03 11:35:25 +0800

    test(handler): test utilities

---
+++ handlers/test.ts
@@ -0,0 +1,34 @@
+import { expect } from "@std/expect";
+import { describe, it } from "@std/testing/bdd";
+
+import { Direction, DIRECTIONS, getBoostDirection } from "@/handlers/utils.ts";
+
+describe("Handlers: utilities", () => {
+  it("returns default if not boosted", () => {
+    const headers = new Headers({
+      "HX-Current-URL": "http://localhost:8000/",
+    });
+    const request = new Request("http://localhost:8000/", { headers });
+
+    expect(getBoostDirection(request)).toBe(DIRECTIONS[0]);
+  });
+  it("determines boosted navigation direction", () => {
+    let headers = new Headers({
+      "HX-Current-URL": "http://localhost:8000/",
+      "HX-Boosted": "true",
+    });
+    let request = new Request("http://localhost:8000/work", { headers });
+
+    let got = getBoostDirection(request), want: Direction = DIRECTIONS[0];
+    expect(got).toBe(want);
+
+    headers = new Headers({
+      "HX-Current-URL": "http://localhost:8000/work",
+      "HX-Boosted": "true",
+    });
+    request = new Request("http://localhost:8000/", { headers });
+
+    got = getBoostDirection(request), want = DIRECTIONS[1];
+    expect(got).toBe(want);
+  });
+});
