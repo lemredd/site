f27278faf5afae98fae1938327c3b11d54917093
Lem Redd <lmoa.jhdp@gmail.com>
2025-01-03 11:36:16 +0800

    feat(handler): add `boostDirection` context

--- handlers/about/index.ts
+++ handlers/about/index.ts
@@ -1,9 +1,13 @@
-import { render } from "@/handlers/utils.ts";
+import { getBoostDirection, render } from "@/handlers/utils.ts";
 import { RouteHandler } from "@/handlers/types.ts";
 
 export const aboutHandler = ((request: Request): Response => {
   return render({
     name: "about/index.html",
-    context: { request, title: "About" },
+    context: {
+      request,
+      title: "About",
+      boostDirection: getBoostDirection(request),
+    },
   });
 }) satisfies RouteHandler;
