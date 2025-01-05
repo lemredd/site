6cd2ab320ba32753adfe69bfaa7b70a13b1fd229
Lem Redd <lmoa.jhdp@gmail.com>
2025-01-05 21:10:35 +0800

    chore(handler): define common metadata
    Signed-off-by: Lem Redd <lmoa.jhdp@gmail.com>

---
+++ handlers/constants.ts
@@ -0,0 +1,36 @@
+import { Metadata } from "@/handlers/types.ts";
+
+export const COMMON_META: Metadata["meta"] = [
+  {
+    nameOrProperty: "name",
+    key: "author",
+    content: "Jarlem Red de Peralta",
+  },
+  {
+    nameOrProperty: "name",
+    key: "keywords",
+    content:
+      "web development, front-end, back-end, full stack, developer, software engineer",
+  },
+  {
+    nameOrProperty: "name",
+    key: "robots",
+    content: "index, follow",
+  },
+  {
+    nameOrProperty: "property",
+    key: "og:image",
+    content: `${Deno.env.get("BASE_URL")}/websiteog.png`,
+  },
+];
+
+export const COMMON_LINKS: Metadata["links"] = [
+  {
+    rel: "apple-touch-icon",
+    href: "/apple-touch-icon.png",
+  },
+  {
+    rel: "manifest",
+    href: "/manifest.json",
+  },
+];
