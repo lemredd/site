6ef0a61385f6339d5387df67e5c2e0cdd9bd569e
Lem Redd <lmoa.jhdp@gmail.com>
2025-01-11 09:07:31 +0800

    chore(type): augment `happy-dom.Window`

---
+++ global.d.ts
@@ -0,0 +1,17 @@
+import type { Window as _Window } from "happy-dom";
+
+declare global {
+  const turnstile: {
+    ready: (callback: () => void) => void;
+    render: (callback: () => void) => void;
+  };
+}
+
+declare module "happy-dom" {
+  interface Window {
+    turnstile: {
+      ready: (callback: () => void) => void;
+      render: (callback: () => void) => void;
+    };
+  }
+}
