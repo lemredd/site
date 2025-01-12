7bbc07c7fde433c9ad41fac7aadde986047ba951
Lem Redd <lmoa.jhdp@gmail.com>
2025-01-12 14:04:50 +0800

    feat(handler): cache static files

--- handlers/index.ts
+++ handlers/index.ts
@@ -25,13 +25,33 @@ const handleByURLPattern = (
   return [new Response("Not Found", { status: 404 }), false];
 };
 
+const staticFileHandler = (async (request: Request) => {
+  const url = new URL(request.url);
+  const STATIC_FILE_CACHE_AGE = 1 * 60 * 60 * 24 * 365;
+  const response = await serveFile(
+    request,
+    `./static${url.pathname}`,
+  );
+
+  response.headers.set(
+    "Cache-Control",
+    `public, max-age=${STATIC_FILE_CACHE_AGE}`,
+  );
+  (url.pathname.endsWith("tailwind.css") || url.pathname.endsWith(".js")) &&
+    response.headers.set(
+      "Cache-Control",
+      "no-cache",
+    );
+
+  return response;
+}) satisfies RouteHandler;
+
 export const mainHandler = (async (request: Request) => {
   const [response, matched] = handleByURLPattern(request);
   if (matched) return await response;
 
   try {
-    const url = new URL(request.url);
-    return await serveFile(request, `./static${url.pathname}`);
+    return await staticFileHandler(request);
   } catch {
     return await response; // 404 by default
   }
