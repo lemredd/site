d0141fa7c4320314721c1111d99bc6e6bf67f8d9
Lem Redd <lmoa.jhdp@gmail.com>
2025-01-12 10:06:27 +0800

    test(html): assert marquee animation reset

--- html/work/test.ts
+++ html/work/test.ts
@@ -10,6 +10,11 @@ import {
   MARQUEE_OBSERVER_ID,
 } from "@/handlers/constants.ts";
 
+import { mainHandler } from "@/handlers/index.ts";
+import { fakeWindowForScript } from "@/handlers/test.ts";
+import { spy } from "@std/testing/mock";
+import { IntersectionObserverEntry } from "happy-dom";
+
 describe("Work: Projects", () => {
   it("displays skeletons initially", () => {
     const rendered = template.render("work/projects.hx.html", {});
@@ -32,16 +37,16 @@ describe("Work: Projects", () => {
 });
 
 describe("Work: Skills", () => {
-  const rendered = template.render("work/index.html", {
-    request: new Request("http://localhost:8000/work"),
-    skills: ["HTML", "CSS", "JavaScript", "TypeScript"],
-    marqueeData: {
-      "marquee-observer-id": MARQUEE_OBSERVER_ID,
-      "marquee-id": MARQUEE_ID,
-      "marquee-dup-id-prefix": MARQUEE_DUP_ID_PREFIX,
-    },
-  });
   it("has marquee-related elements", () => {
+    const rendered = template.render("work/index.html", {
+      request: new Request("http://localhost:8000/work"),
+      skills: ["HTML", "CSS", "JavaScript", "TypeScript"],
+      marqueeData: {
+        "marquee-observer-id": MARQUEE_OBSERVER_ID,
+        "marquee-id": MARQUEE_ID,
+        "marquee-dup-id-prefix": MARQUEE_DUP_ID_PREFIX,
+      },
+    });
     const $ = load(rendered);
 
     expect($(`#${MARQUEE_OBSERVER_ID}`).length).toBe(1);
@@ -55,5 +60,48 @@ describe("Work: Skills", () => {
       marqueeDupIdPrefix: MARQUEE_DUP_ID_PREFIX,
     };
     expect(gotScriptDataset).toEqual(wantScriptDataset);
+  });
+
+  it("resets marquee animation", async () => {
+    const module = await mainHandler(
+      new Request("http://localhost:8000/marquee.js"),
+    ).then((res) => res.text());
+
+    const { window, document, script, appendScript } = fakeWindowForScript(
+      module,
+    );
+
+    script.dataset.marqueeObserverId = MARQUEE_OBSERVER_ID;
+    const marqueeObserverElement = document.createElement("div");
+    marqueeObserverElement.id = MARQUEE_OBSERVER_ID;
+    document.body.appendChild(marqueeObserverElement);
+
+    script.dataset.marqueeId = MARQUEE_ID;
+    const marqueeElement = document.createElement("div");
+    marqueeElement.id = MARQUEE_ID;
+    document.body.appendChild(marqueeElement);
+
+    script.dataset.marqueeDupIdPrefix = MARQUEE_DUP_ID_PREFIX;
+    const marqueeDupElement = document.createElement("div");
+    marqueeDupElement.id = `${MARQUEE_DUP_ID_PREFIX}1`;
+    document.body.appendChild(marqueeDupElement);
+
+    using intersectionObserverSpy = spy(window, "IntersectionObserver");
+    using requestAnimationFrameSpy = spy(window, "requestAnimationFrame");
+    using marqueeElementRemoveAttrSpy = spy(marqueeElement, "removeAttribute");
+    appendScript();
+
+    const gotIntersectionObserverCallback = intersectionObserverSpy.calls[0]
+      .args[0] as (entries: IntersectionObserverEntry[]) => void;
+    const intersectionObserverEntry = new IntersectionObserverEntry({
+      isIntersecting: false,
+    });
+
+    gotIntersectionObserverCallback([intersectionObserverEntry]);
+    expect(marqueeElement.style.animation).toBe("none");
+    await window.happyDOM.waitUntilComplete();
+
+    expect(requestAnimationFrameSpy.calls).toHaveLength(1);
+    expect(marqueeElementRemoveAttrSpy.calls).toHaveLength(1);
   });
 });
