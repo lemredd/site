c453f0cc2e345399878f76cca5ddf36dcfc87cdb
Lem Redd <lmoa.jhdp@gmail.com>
2025-01-13 21:53:13 +0800

    chore(type): declare service worker types

--- global.d.ts
+++ global.d.ts
@@ -10,9 +10,22 @@ declare global {
 
 declare module "happy-dom" {
   interface Window {
+    importScripts: (url: string) => void;
     turnstile: {
       ready: (callback: () => void) => void;
       render: (callback: () => void) => void;
+    };
+    workbox: {
+      setConfig: (config: Record<string | number | symbol, unknown>) => void;
+      routing: {
+        registerRoute: (
+          callback: (context: Record<string, unknown>) => boolean,
+          strategy: unknown,
+        ) => void;
+      };
+      strategies: {
+        CacheFirst: () => unknown;
+      };
     };
   }
 }
