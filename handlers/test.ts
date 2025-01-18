199856a08562d28272a44753359052215a3b6cac
Lem Redd <lmoa.jhdp@gmail.com>
2025-01-18 11:24:39 +0800

    refactor(test): move tests

--- handlers/test.ts
+++ handlers/test.ts
@@ -1,7 +1,40 @@
 import { expect } from "@std/expect";
 import { describe, it } from "@std/testing/bdd";
 
+import { mainHandler } from "@/handlers/index.ts";
 import { Direction, DIRECTIONS, getBoostDirection } from "@/handlers/utils.ts";
+
+describe("Main Handler", () => {
+  it("returns 404", async () => {
+    const response = await mainHandler(
+      new Request("http://localhost:8000/unknown"),
+    );
+
+    expect(response.status).toBe(404);
+  });
+
+  it("serves static files with cache control", async () => {
+    const response = await mainHandler(
+      new Request("http://localhost:8000/favicon.ico"),
+    );
+
+    await response.blob(); // Close the response
+    expect(response.status).toBe(200);
+
+    expect(response.headers.get("Cache-Control")).toBe(
+      "public, max-age=31536000",
+    );
+  });
+
+  it("serves static `tailwind.css` and JS files with `no-cache`", async () => {
+    const response = await mainHandler(
+      new Request("http://localhost:8000/tailwind.css"),
+    );
+
+    await response.blob(); // Close the response
+    expect(response.headers.get("Cache-Control")).toBe("no-cache");
+  });
+});
 
 describe("Handlers: utilities", () => {
   it("returns empty string if not boosted", () => {
